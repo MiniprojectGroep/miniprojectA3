7b309a3436648a7760754defc1dea413d24a60d9
Nickernator <nickwindt@hotmail.nl>
2016-11-02 09:58:47 +0100

    Update API.py
    Probleem bij de API opgelost

--- API.py
+++ API.py
@@ -1,16 +1,24 @@
-import requests
-import datetime
+def getAPIDataToXML():
+    import requests
+    import datetime
 
-private_key = ('lo1wkfqu66uwntl7nkvim1b2b8yoqdz6')
-file = 'films.xml'
-nu = datetime.datetime.now()
-datum = nu.strftime('%d-%m-%Y')
+    private_key = ('lo1wkfqu66uwntl7nkvim1b2b8yoqdz6')
+    file = 'films.xml'
+    nu = datetime.datetime.now()
+    datum = nu.strftime('%d-%m-%Y')
 
-# methode 0 = alle films, method 1 = filmtips, method 2 = film van de dag
-methode = 0
-link = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey={}&dag={}&sorteer={}'.format(private_key,datum,methode)
+    # methode 0 = alle films, method 1 = filmtips, method 2 = film van de dag
+    methode = 0
+    link = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey={}&dag={}&sorteer={}'.format(private_key,datum,methode)
 
-response = requests.get(link)
+    response = requests.get(link)
 
-with open(file,'w')as myXMLFile:
-    myXMLFile.write(response.text)
+    str = response.text
+ # In de API wordt er soms (1-11-2016 en 2-11-2016) een ' ; ' in het XML geleverd, als deze niet verwijdert wordt dan kan dit ervoor zorgen dat het XML bestand onleesbaar wordt
+    str = str.replace('&','')
+    str = str.replace(';','')
+    with open(file,'w')as filmsXML:
+        try:
+            filmsXML.write(str)
+        except:
+            print('Error in de API')

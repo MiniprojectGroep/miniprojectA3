91b15c7da7f90a27a9daa56ab6ebedcb20acaf55
Nickernator <nickwindt@hotmail.nl>
2016-11-03 21:44:48 +0100

    Update sql.py
    Meer functies voor sql.py

--- sql.py
+++ sql.py
@@ -13,15 +13,11 @@ def startDatabase(databasename):
         c = connect.cursor()
         # Create table
 
-        #
         c.execute('''CREATE TABLE IF NOT EXISTS accounts
                          (gebruikersnaam text, email text, wachtwoord text, filmnaam text, filmstarttijd text, type text)''')
 
-        c.execute('''CREATE TABLE IF NOT EXISTS aanbieders
-                         (aanbieder text UNIQUE, wachtwoord text, type text)''')
-
         c.execute('''CREATE TABLE IF NOT EXISTS films
-                        (filmnaam text UNIQUE, aanbieder text, aantal_bezoekers int)''')
+                        (filmnaam text, aanbiedersnaam text, filmstarttijd text, aantal_bezoekers int)''')
 
 def isLoginCorrect(gebruikersnaam, wachtwoord):
     import sqlite3
@@ -36,17 +32,88 @@ def isLoginCorrect(gebruikersnaam, wachtwoord):
 
         # controleer of gebruikersnaam EN wachtwoord overeenkomen met de opgegeven data, eigenlijk overbodig maar dubbel check
         for resultaat in resultaten:
-            if resultaat[0] == gebruikersnaam and resultaat[1] == wachtwoord:
+
+            if resultaat[0] == gebruikersnaam and resultaat[2] == wachtwoord:
                 connect.close()
                 # data van gebruikersnaam en wachtwoord wissen ivm security redenen
-                gebruikersnaam = None
-                wachtwoord = None
 
                 return True
         return False
 
+def isFilmInDatabase(film):
+    import sqlite3
+    database = 'Thuisbioscoop.db'
+    if isDatabaseConnection(database) == True:
+        connect = sqlite3.connect(database)
+        c = connect.cursor()
+        gegevens = [film]
+
+        c.execute('''SELECT filmnaam FROM films WHERE filmnaam = ? ''',gegevens)
+        resultaten = c.fetchall()
+
+        # controleer of gebruikersnaam EN wachtwoord overeenkomen met de opgegeven data, eigenlijk overbodig maar dubbel check
+        for resultaat in resultaten:
+            if resultaat[0] == film:
+                connect.close()
+                # data van gebruikersnaam en wachtwoord wissen ivm security redenen
+                return True
+        return False
+
+def createFilmsTableData(lst):
+    import sqlite3
+    database = 'Thuisbioscoop.db'
+    if isDatabaseConnection(database) == True:
+        connect = sqlite3.connect(database)
+        c = connect.cursor()
+        for films in lst:
+            if isFilmInDatabase(films[0]) == False:
+                c.execute('INSERT INTO films VALUES (?,?,?,?)', films)
+        connect.commit()
+        return True
+
+def getGebruikerFilmEnTijd(gebruikersnaam,wachtwoord):
+    import sqlite3
+    database = 'Thuisbioscoop.db'
+    if isDatabaseConnection(database) == True:
+        connect = sqlite3.connect(database)
+        c = connect.cursor()
+        gegevens = [gebruikersnaam, wachtwoord]
+
+        c.execute('''SELECT gebruikersnaam,wachtwoord,filmnaam,filmstarttijd FROM accounts WHERE gebruikersnaam = ? AND wachtwoord = ?''',gegevens)
+        resultaten = c.fetchall()
+
+        # controleer of gebruikersnaam EN wachtwoord overeenkomen met de opgegeven data
+        for resultaat in resultaten:
+
+            if resultaat[0] == gebruikersnaam and resultaat[1] == wachtwoord:
+                connect.close()
+                lst = []
+                lst.append(resultaat[2])
+                lst.append(resultaat[3])
+                return lst
+    return False
+
+def isCodeUnique(code):
+    import sqlite3
+    database = 'Thuisbioscoop.db'
+    if isDatabaseConnection(database) == True:
+        connect = sqlite3.connect(database)
+        c = connect.cursor()
+        gegevens = [code]
+
+        c.execute('''SELECT wachtwoord FROM accounts WHERE wachtwoord = ?''',gegevens)
+        resultaten = c.fetchall()
+
+        # controleer of gebruikersnaam EN wachtwoord overeenkomen met de opgegeven data
+        for resultaat in resultaten:
+            if resultaat == code:
+                connect.close()
+                return False
+        return True
+    return False
+
 def registreerGebruiker(lst):
-    try:
+    #try:
         import sqlite3
         database = 'Thuisbioscoop.db'
         if isDatabaseConnection(database) == True:
@@ -58,5 +125,42 @@ def registreerGebruiker(lst):
             return True
         else:
             return False
-    except:
+    #except:
+        #return False
+
+def registeerAanbieders():
+    aanbieders = [
+        ['Nick','nick@hotmail.nl','kaasbroodje13','','','aanbieder'],
+        ['Piet Jan', 'pietjan@gmail.com','arash','','','aanbieder'],
+        ['Nathan', 'nathan@gmail.com','pizzaburger','','','aanbieder'],
+        ['Jeroen','jeroen@hotmail.nl','computerlaptop','','','aanbieder']
+    ]
+    import sqlite3
+    database = 'Thuisbioscoop.db'
+    if isDatabaseConnection(database) == True:
+        connect = sqlite3.connect(database)
+        c = connect.cursor()
+        print(aanbieders)
+        for aanbieder in aanbieders:
+            print(aanbieder)
+            if isAanbiederInDatabase(aanbieder[0]) == False:
+                c.execute('INSERT INTO accounts VALUES (?,?,?,?,?,?)', aanbieder)
+        connect.commit()
+        return True
+    return False
+
+def isAanbiederInDatabase(aanbieder):
+    import sqlite3
+    database = 'Thuisbioscoop.db'
+    if isDatabaseConnection(database) == True:
+        connect = sqlite3.connect(database)
+        c = connect.cursor()
+        gegevens = [aanbieder]
+        
+        c.execute('''SELECT gebruikersnaam FROM accounts WHERE gebruikersnaam = ? AND type == 'aanbieder' ''',gegevens)
+        resultaten = c.fetchall()
+
+        for resultaat in resultaten:
+            if resultaat[0] == aanbieder:
+                return True
         return False

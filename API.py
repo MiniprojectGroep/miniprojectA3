2e563e5dd1a4ee5332df40c82cc8a7c77432ec50
Nickernator <nickwindt@hotmail.nl>
2016-11-03 22:42:46 +0100

    Update API.py

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-def getAPIDataToXML():
+def getAPIDataToXML(): # Met dank aan Arash, verbetert door Nick i.v.m. XML problemen
     import requests
     import datetime
 
@@ -10,17 +10,17 @@ def getAPIDataToXML():
     # methode 0 = alle films, method 1 = filmtips, method 2 = film van de dag
     methode = 0
     link = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey={}&dag={}&sorteer={}'.format(private_key,datum,methode)
-
     response = requests.get(link)
 
     string = response.text
- # In de API wordt er soms (1-11-2016 en 2-11-2016) een ' ; ' ,of er zit een encoding fout in het bestand, in het XML geleverd, als deze niet verwijdert of aangepast worden dan kan dit ervoor zorgen dat het XML bestand onleesbaar wordt
+ # In de API wordt er soms (1-11-2016 en 2-11-2016 en 3-11-2016, zeer waarschijnlijk nogsteeds op 4-11-2016) een ' ; ' ,of er zit een encoding fout in het bestand, in het XML geleverd, als deze niet verwijdert of aangepast worden dan kan dit ervoor zorgen dat het XML bestand onleesbaar wordt
+ # Het bovenstaande probleem wordt er gebruik gemaakt van replace, over een interne oplossing in de API is contact opgenomen met de developer van de API   
     string = string.replace('&eacute;', 'é') # &eacure; staat voor ' é '
     string = string.replace('&euml;','ë') # &euml; staat voor ' ë '
     string = string.replace('<?xml version="1.0" encoding="iso-8859-1"?>','<?xml version="1.0" encoding="UTF-8"?>' ) # zorgt ook voor en error??
     string = string.replace(';','') # Haal onbekende encodingselementen weg zodat het XML kan blijven functioneren
     string = string.replace('&','')
-    string = string.replace('','') # Haal onbekende  tekensweg
+    string = string.replace('','') # Haal onbekende tekensweg
     new_str = str(string)
     with open(file,'w')as filmsXML:
         try:

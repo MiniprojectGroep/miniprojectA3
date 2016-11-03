5b1817358b53ee2b5eecd6cac06d9367c80a0f77
NathanH1250 <Nathan.haseth@student.hu.nl>
2016-11-03 13:46:35 +0100

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -19,6 +19,7 @@ iets = [('2006-03-28', 'Woerden', 'Harmelen', 45.00),
              ('2006-04-06', 'Woerden', 'Utrecht Centraal', 53.00),
             ]
 c.executemany('INSERT INTO treinen VALUES (?,?,?,?)', iets)
+
 '''
 
 gebruiker = ''
@@ -38,17 +39,26 @@ connect.commit()
 # Just be sure any changes have been committed or they will be lost.
 connect.close()
 
-CREATE TABLE (id INTEGER, bezoeker TEXT, film INTEGER)
+c.execute('''CREATE TABLE (id INTEGER, bezoeker TEXT, film INTEGER)''')
 
-INSERT INTO bezoeker VALUES (1, "Jack Pieters", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (2, "Klaas Bergjes", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (3, "Jacob Ozonlaag", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (4, "Piet Dikkiedik", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (5, "Nick Scheet", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (6, "Jan Jamal", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (7, "Frank Spijkerdop", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (8, "Alberto Spaghetti", 'filmoffilmtijdhier');
-INSERT INTO bezoeker VALUES (9, "Gert Samson", 'filmoffilmtijdhier')
-INSERT INTO bezoeker VALUES (10, "Benny Vogelhuis", 'filmoffilmtijdhier')
+table = accounts
 
+c.execute('''CREATE TABLE IF NOT EXISTS accounts
+             (id integer, volle naam text, film text)''')
+
+'''
+bezoek = [(1, "Jack Pieters", 'filmoffilmtijdhier'),
+            (2, "Klaas Bergjes", 'filmoffilmtijdhier'),
+            (3, "Jacob Ozonlaag", 'filmoffilmtijdhier'),
+            (4, "Piet Dikkiedik", 'filmoffilmtijdhier'),
+            (5, "Nick Scheet", 'filmoffilmtijdhier'),
+            (6, "Jan Jamal", 'filmoffilmtijdhier'),
+            (7, "Frank Spijkerdop", 'filmoffilmtijdhier'),
+            (8, "Alberto Spaghetti", 'filmoffilmtijdhier'),
+            (9, "Gert Samson", 'filmoffilmtijdhier'),
+            (10, "Benny Vogelhuis", 'filmoffilmtijdhier'),
+            ]
+c.executemany('INSERT INTO bezoeker VALUES (?,?,?)', bezoek)
+'''
+            
 SELECT 4 from bezoeker;

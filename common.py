3030a5e5eb9eeb943226481e2c55e2a4152a6853
Nickernator <nickwindt@hotmail.nl>
2016-11-04 12:31:23 +0100

    Update common.py
    Definitieve versie API.py

--- common.py
+++ common.py
@@ -1,30 +1,28 @@
-# Lijst van alle algemene functies
+def getAPIDataToXML():
+    import requests
+    import datetime
 
-# Krijg een lijst met alle films van vandaag
-def getFilms(bestand):
-    import xmltodict
+    private_key = ('lo1wkfqu66uwntl7nkvim1b2b8yoqdz6')
+    file = 'films.xml'
+    nu = datetime.datetime.now()
+    datum = nu.strftime('%d-%m-%Y')
 
-    def processXML(filename):
-        with open(filename) as myXMLFile:
-            filecontentstring = myXMLFile.read()
-            xmldictionary = xmltodict.parse(filecontentstring)
-            return xmldictionary
+    # methode 0 = alle films, method 1 = filmtips, method 2 = film van de dag
+    methode = 0
+    link = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey={}&dag={}&sorteer={}'.format(private_key,datum,methode)
+    response = requests.get(link)
 
-    filmdict = processXML(bestand)
-    films = filmdict["filmsoptv"]["film"]
-
-    # lst[x][0] = titel, lst[x][1] = genre, lst[x][2] = jaar, lst[x][3] = imdb_rating, lst[x][4] = afbeelding_URL
-
-    lst = []
-    film_nummer = 1
-    for film in films:
-        temp_list = []
-        temp_list.append(str(film['titel']))
-        temp_list.append(str(film['genre']))
-        temp_list.append(str(film['jaar']))
-        temp_list.append(str(film['imdb_rating']))
-        temp_list.append(str(film['cover']))
-        temp_list.append(film_nummer)
-        lst.append(temp_list)
-        film_nummer += 1
-    return lst
+    string = response.text
+ # In de API wordt er soms (1-11-2016 en 2-11-2016) een ' ; ' ,of er zit een encoding fout in het bestand, in het XML geleverd, als deze niet verwijdert of aangepast worden dan kan dit ervoor zorgen dat het XML bestand onleesbaar wordt
+    string = string.replace('&eacute;', 'é') # &eacure; staat voor ' é '
+    string = string.replace('&euml;','ë') # &euml; staat voor ' ë '
+    string = string.replace('<?xml version="1.0" encoding="iso-8859-1"?>','<?xml version="1.0" encoding="UTF-8"?>' ) # zorgt ook voor en error??
+    string = string.replace(';','') # Haal onbekende encodingselementen weg zodat het XML kan blijven functioneren
+    string = string.replace('&','')
+    string = string.replace('','') # Haal onbekende tekensweg
+    new_str = str(string)
+    with open(file,'w')as filmsXML:
+        try:
+            filmsXML.write(new_str)
+        except:
+            print('API kon niet succesvol worden opgehaalt')

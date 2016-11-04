f5c0fbe19e8ad3041ad530f1fa355dc89bc0629c
Nickernator <nickwindt@hotmail.nl>
2016-11-04 12:32:47 +0100

    Update common.py
    Per ongeluk API.py hier geplaatst, nu correct

--- common.py
+++ common.py
@@ -1,28 +1,98 @@
-def getAPIDataToXML():
-    import requests
+# Alle algemene functies
+
+# Krijg een lijst met alle films van vandaag
+def getFilms(bestand):
+    import xmltodict
     import datetime
 
-    private_key = ('lo1wkfqu66uwntl7nkvim1b2b8yoqdz6')
-    file = 'films.xml'
-    nu = datetime.datetime.now()
-    datum = nu.strftime('%d-%m-%Y')
+    datetime.datetime.fromtimestamp(int("1284101485")).strftime('%Y-%m-%d %H:%M:%S')
 
-    # methode 0 = alle films, method 1 = filmtips, method 2 = film van de dag
-    methode = 0
-    link = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey={}&dag={}&sorteer={}'.format(private_key,datum,methode)
-    response = requests.get(link)
 
-    string = response.text
- # In de API wordt er soms (1-11-2016 en 2-11-2016) een ' ; ' ,of er zit een encoding fout in het bestand, in het XML geleverd, als deze niet verwijdert of aangepast worden dan kan dit ervoor zorgen dat het XML bestand onleesbaar wordt
-    string = string.replace('&eacute;', 'é') # &eacure; staat voor ' é '
-    string = string.replace('&euml;','ë') # &euml; staat voor ' ë '
-    string = string.replace('<?xml version="1.0" encoding="iso-8859-1"?>','<?xml version="1.0" encoding="UTF-8"?>' ) # zorgt ook voor en error??
-    string = string.replace(';','') # Haal onbekende encodingselementen weg zodat het XML kan blijven functioneren
-    string = string.replace('&','')
-    string = string.replace('','') # Haal onbekende tekensweg
-    new_str = str(string)
-    with open(file,'w')as filmsXML:
-        try:
-            filmsXML.write(new_str)
-        except:
-            print('API kon niet succesvol worden opgehaalt')
+    def processXML(filename):
+        with open(filename) as myXMLFile:
+            filecontentstring = myXMLFile.read()
+            xmldictionary = xmltodict.parse(filecontentstring)
+            return xmldictionary
+
+    filmdict = processXML(bestand)
+    films = filmdict["filmsoptv"]["film"]
+
+    # lst[x][0] = titel, lst[x][1] = genre, lst[x][2] = jaar, lst[x][3] = imdb_rating, lst[x][4] = afbeelding_URL, lst[x][5] = film_nummer (Wordt gebruikt voor ListBox), lst[x][6] = Zender, lst[x][7] = start tijd, lst[x][8] = eindtijd
+
+    lst = []
+    film_nummer = 1
+    for film in films:
+        temp_list = []
+        temp_list.append(str(film['titel']))
+        temp_list.append(str(film['genre']))
+        temp_list.append(str(film['jaar']))
+        temp_list.append(str(film['imdb_rating']))
+        temp_list.append(str(film['cover']))
+        temp_list.append(film_nummer)
+        temp_list.append(str(film['zender']))
+
+        starttijd = str(datetime.datetime.fromtimestamp(int(film['starttijd'])).strftime('%H:%M'))
+        eindtijd = str(datetime.datetime.fromtimestamp(int(film['eindtijd'])).strftime('%H:%M'))
+
+        temp_list.append(str(starttijd))
+        temp_list.append(str(eindtijd))
+        lst.append(temp_list)
+        film_nummer += 1
+
+    return lst
+
+def getFilmsNames(bestand):
+    import xmltodict
+    import datetime
+
+    def processXML(filename):
+        with open(filename) as myXMLFile:
+            filecontentstring = myXMLFile.read()
+            xmldictionary = xmltodict.parse(filecontentstring)
+            return xmldictionary
+
+    filmdict = processXML(bestand)
+    films = filmdict["filmsoptv"]["film"]
+
+    # lst[x][0] = titel, lst[x][1] = genre, lst[x][2] = jaar, lst[x][3] = imdb_rating, lst[x][4] = afbeelding_URL, lst[x][5] = film_nummer (Wordt gebruikt voor ListBox), lst[x][6] = Zender, lst[x][7] = start tijd, lst[x][8] = eindtijd
+
+    lst = []
+    film_nummer = 1
+    for film in films:
+        temp_list = []
+        temp_list.append(str(film['titel']))
+        temp_list.append(film_nummer)
+        lst.append(temp_list)
+        film_nummer += 1
+
+    return lst
+
+def getFilmTableDataList(bestand):
+    import xmltodict
+    import datetime
+
+    def processXML(filename):
+        with open(filename) as myXMLFile:
+            filecontentstring = myXMLFile.read()
+            xmldictionary = xmltodict.parse(filecontentstring)
+            return xmldictionary
+
+    filmdict = processXML(bestand)
+    films = filmdict["filmsoptv"]["film"]
+
+    # lst[x][0] = titel, lst[x][1] = Aanbieder, lst[x][2] = start tijd, lst[x][3] = filmdatum lst[x][4] = aantal_bezoekers
+    lst = []
+    for film in films:
+        temp_list = []
+        starttijd = str(datetime.datetime.fromtimestamp(int(film['starttijd'])).strftime('%H:%M'))
+        vandaag = datetime.datetime.now()
+        datum = vandaag.strftime('%d-%m-%Y')
+
+        temp_list.append(str(film['titel']))
+        temp_list.append('')
+        temp_list.append(starttijd)
+        temp_list.append(datum)
+        temp_list.append(0)
+        lst.append(temp_list)
+
+    return lst
